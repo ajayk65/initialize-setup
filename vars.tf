c0a22dc080706bd0045ec5fcee5e0b4f1b02f48c
ajayk65 <72610962+ajayk65@users.noreply.github.com>
2020-10-14 13:14:13 +0100

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,4 +1,4 @@
 variable "env" {
-  description = "Enter the environment where to deploy backend (e.g. dev, prod)"
+  description = "Enter the environment where to deploy backend (e.g. dev,test, prod)"
   type = "string"
 }
